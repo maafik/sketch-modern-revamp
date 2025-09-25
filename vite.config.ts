3f3008e82448f44849da57729c799ccad51b68c5
maafik <maafik@66.ru>
2025-09-25 03:43:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sketch-modern-revamp/', // ⚠️ замените на своё
+  base: '/', // ⚠️ замените на своё
 })
